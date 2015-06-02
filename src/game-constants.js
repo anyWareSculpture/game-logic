979401ca209d0a2cb8f70976e6a98e70ed7a837e
Sunjay Varma <varma.sunjay@gmail.com>
2015-06-02 01:58:42 -0400

    Added most of the knock game logic

---
+++ src/game-constants.js
@@ -0,0 +1,7 @@
+export default {
+  // Flux Action Types
+  ACTION_TYPE_CHANGE_KNOCK_GAME: 'change-knock-pattern',
+
+  // Events
+  EVENT_CHANGE: 'change'
+};
--- src/knock-game-store.js
+++ src/knock-game-store.js
@@ -1,7 +1,64 @@
 const events = require('events');
 
-class KnockGameStore extends events.EventEmitter {
-  constructor() {
+const GameConstants = require('./game-constants');
+const DataChangeTracker = require('./data-change-tracker');
+
+const KNOCK_PATTERNS = {
+  shaveAndHaircut: [0, 447, 318, 152, 450]
+};
+const KNOCK_PATTERN = KNOCK_PATTERNS.shaveAndHaircut;
+
+export default class KnockGameStore extends events.EventEmitter {
+  /**
+   * Creates a store to be used with a flux dispatcher
+   * Emits a change event whenever one or more properties change
+   * so that view-controllers can update
+   * @param {Dispatcher} dispatcher - A dispatcher that implements flux's dispatcher API
+   */
+  constructor(dispatcher) {
     super();
+
+    this.data = new DataChangeTracker({
+      complete: false,
+      pattern: null
+    });
+    this.dispatchToken = null;
+
+    this.registerDispatcherCallbacks(dispatcher);
+  }
+
+  /**
+   * Sends the initial knock pattern to the provided dispatcher
+   * @param {Dispatcher} dispatcher - A dispatcher that implements flux's dispatcher API
+   */
+  static sendInitialKnockPattern(dispatcher) {
+    dispatcher.dispatch({
+      actionType: GameConstants.ACTION_TYPE_CHANGE_KNOCK_GAME,
+      pattern: KNOCK_PATTERN
+    });
+  }
+
+  /**
+   * Registers dispatcher callbacks on the provided dispatcher.
+   * Automatically called by the constructor.
+   * @param {Dispatcher} dispatcher - A dispatcher that implements flux's dispatcher API
+   */
+  registerDispatcherCallbacks(dispatcher) {
+    this.dispatchToken = dispatcher.register(this._handleDispatcherPayload.bind(this));
+  }
+
+  _handleDispatcherPayload(payload) {
+    if (payload.actionType === GameConstants.ACTION_TYPE_CHANGE_KNOCK_GAME) {
+      this.data.set("pattern", payload.pattern);
+    }
+
+    this._emitChanges();
+  }
+
+  _emitChanges() {
+    const changes = this.data.getChangedCurrentValues();
+    this.data.clearChanges();
+
+    this.emit(GameConstants.EVENT_CHANGE, changes);
   }
 }
