41b97f70ceefb465e44dc8217fb697c882716edc
Sunjay Varma <varma.sunjay@gmail.com>
2015-06-20 11:40:36 -0400

    Some of the implementation of the sculpture store

--- src/sculpture-store.js
+++ src/sculpture-store.js
@@ -1,10 +1,37 @@
 const events = require('events');
 
+const MoleGameLogic = require('./logic/mole-game-logic');
+const LightArray = require('./utils/light-array');
 const TrackedData = require('./utils/tracked-data');
+
+const STATE_READY = "ready";
+const STATE_ERROR = "error";
+const STATE_SUCCESS = "success";
+const STATE_SUCCESS = "locked";
 
 export default class SculptureStore extends events.EventEmitter {
   constructor(dispatcher) {
+    super();
+
     this.data = new TrackedData({
+      'state': STATE_READY,
+      'lights': new LightArray({
+        // stripId : number of panels
+        '0': 10,
+        '1': 10,
+        '2': 10
+      }),
+      'mole': new TrackedData(MoleGameLogic.trackedProperties)
     });
+
+    this._registerDispatcher(dispatcher);
+  }
+
+  _registerDispatcher(dispatcher) {
+    dispatcher.register(this._handleActionPayload.bind(this));
+  }
+
+  _handleActionPayload(payload) {
+    console.log(payload);
   }
 }
