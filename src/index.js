41cad1439f21a111a0cca5f748c0eebae01ea9db
Sunjay Varma <varma.sunjay@gmail.com>
2015-06-23 14:21:16 -0400

    Fixed game logic so it exposes the correct modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export const GameConstants = require('./game-constants');
-export const KnockGameStore = require('./stores/knock-game-store');
+export const SculptureStore = require('./sculpture-store');
+export const PanelsActionCreator = require('./src/actions/panels-action-creator');
+export const MoleGameLogic = require('./src/logic/mole-game-logic');
