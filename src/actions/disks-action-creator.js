71417163407fef28ac36e20fe220aeca8702d705
Sunjay Varma <varma.sunjay@gmail.com>
2015-07-03 16:25:18 -0400

    Added action creator for disks

---
+++ src/actions/disks-action-creator.js
@@ -0,0 +1,33 @@
+const BaseActionCreator = require('./base-action-creator');
+
+export default class DisksActionCreator extends BaseActionCreator {
+  // Action types
+  static DISK_UPDATE = "disk-update";
+
+  static enabledWhileSculptureLocked() {
+    return [];
+  }
+
+  /**
+   * Sends an action to the dispatcher representing when a disk position, direction, or state changes.
+   * Only sends action if at least one argument is provided to the object
+   */
+  sendDiskUpdate({position=null, direction=null, state=null}) {
+    const payloadBody = {};
+    if (position !== null) {
+      payloadBody[position] = position;
+    }
+
+    if (direction !== null) {
+      payloadBody[direction] = direction;
+    }
+
+    if (state !== null) {
+      payloadBody[state] = state;
+    }
+    
+    if (Object.keys(payloadBody).length > 0) {
+      this._dispatch(DisksActionCreator.DISK_UPDATE, payloadBody);
+    }
+  }
+}
