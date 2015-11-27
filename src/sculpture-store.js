d66e95220e47da3b8a1a2a38516f58a7994a6cbc
Sunjay Varma <varma.sunjay@gmail.com>
2015-11-27 15:15:22 -0500

    Updated unsaved reference

--- src/sculpture-store.js
+++ src/sculpture-store.js
@@ -40,7 +40,7 @@ export default class SculptureStore extends events.EventEmitter {
         [this.config.LIGHTS.PERIMETER_STRIP]: 6,
         [this.config.LIGHTS.DISK_LIGHT_STRIP]: 3,
         [this.config.LIGHTS.HANDSHAKE_STRIP]: 4,
-        [this.config.LIGHTS.ENVIRONMENT_STRIP]: 3
+        [this.config.LIGHTS.ART_LIGHTS_STRIP]: 3
       }),
       disks: new TrackedData({
         disk0: new Disk(),
