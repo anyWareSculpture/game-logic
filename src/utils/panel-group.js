f3d6ffcaa46f2102e58ec90d1fdc41e219c213c0
Marius Kintel <marius@kintel.net>
2015-12-21 13:51:42 -0500

    removed unused code, doc

--- src/utils/panel-group.js
+++ src/utils/panel-group.js
@@ -1,5 +1,14 @@
 const TrackedData = require('./tracked-data');
 
+/**
+ * Tracks panel state.
+ * Since we don't support removing tracked data, we need to introduce an OFF state
+ * This is currently meant for the mole game, where we want to know if an "on" state 
+ * is active or turned to location color (and thus ignored)
+ * 
+ * Note: Since we require a string key, we use the combined stripId and panelId
+ * as a key.
+ */
 export class TrackedPanels extends TrackedData {
 
   static STATE_ON = "on";
@@ -23,36 +32,6 @@ export class TrackedPanels extends TrackedData {
   }
 
   _hash(stripId, panelId) {
-    return `${stripId},${panelId}`;
-  }
-}
-
-export class PanelSet {
-  constructor() {
-    this._set = {};
-  }
-
-  addPanel(panel) {
-    this._set[this._hash(panel)] = panel;
-  }
-
-  hasPanel(panel) {
-    return this._set.hasOwnProperty(this._hash(panel));
-  }
-
-  deletePanel(panel) {
-    delete this._set[this._hash(panel)];
-  }
-
-  get size() {
-    return Object.keys(this._set).length;
-  }
-
-  *values() {
-    for (let key of Object.values(this._set)) yield key;
-  }
-
-  _hash([stripId, panelId]) {
     return `${stripId},${panelId}`;
   }
 }
